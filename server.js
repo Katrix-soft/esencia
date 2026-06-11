05e80aeb36080363ff3b3a8ab6c7972aeb23dee3
nachaxed <igexsanchez@gmail.com>
2026-06-11 00:06:08 -0300

    feat: configure robust CORS headers to support third-party application requests

--- server.js
+++ server.js
@@ -39,7 +39,12 @@ const generalLimiter = rateLimit({
 });
 app.use('/api', generalLimiter);
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-signature', 'x-request-id'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
